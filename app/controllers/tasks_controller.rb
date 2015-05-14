c4f12b67fb558e1f9877170526b3da5f9b331c3f
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-05-14 15:25:33 -0400

    Add change task state condition

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
 def index
     @to_do = current_user.tasks.where(state: "to_do")
@@ -53,6 +53,13 @@ def new
     respond_to do |format|
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
+    end
+  end
+
+  def change
+    @task.update_attributes(state: params[:state])
+    respond_to do |format|
+      format.html {redirect_to tasks_path, notice: "Task Update"}
     end
   end
 
